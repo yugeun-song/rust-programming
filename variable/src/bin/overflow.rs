7f0895696176b522c673bf500f211f31ecc000a5
Yugeun Song <yugeunsong2002@gmail.com>
2026-09-23 07:16:49 +0900

    feat: add overflow and statement_allocation to variable

---
+++ variable/src/bin/overflow.rs
@@ -0,0 +1,17 @@
+use std::panic;
+
+fn do_overflow() {
+    let mut x: u8 = u8::MAX;
+    println!("just trying to add one...");
+    x += 1;
+    println!("x is {x}");
+}
+
+fn main() {
+    let result = panic::catch_unwind(|| do_overflow());
+
+    match result {
+        Ok(_) => println!("completed normally!"),
+        Err(_) => println!("panic caught!"),
+    }
+}
---
+++ variable/src/bin/statement_allocation.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let result = {
+        let dummy = 10;
+        (dummy * 20)
+    };
+
+    println!("y is {}", result);
+}
